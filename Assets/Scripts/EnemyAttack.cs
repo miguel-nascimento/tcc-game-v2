f8a3bb0b768837500b308a048dec5b0ae9af5660
miguel-nascimento <miguelgomes13@live.com>
2020-10-27 23:14:17 -0300

    Enemy attack delay

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-     public float damage, attackRange;
-
-     public LayerMask playerLayer;
-     public Transform Hitbox;
+    public float damage, attackRange;
+    public LayerMask playerLayer;
+    public Transform Hitbox;
+    public float cooldownTimer = 1;
+    private float nextAttackTimer = 0;
 
     // Update is called once per frame
     void Update()
     {
         Collider2D[] hitEnimies = Physics2D.OverlapCircleAll(Hitbox.position, attackRange, playerLayer);
         foreach(Collider2D enemy in hitEnimies){
-            enemy.GetComponent<PlayerMovement>().TakeDamage(damage);
-            Debug.Log("hit " + enemy.name);
-        
+            if(Time.time > nextAttackTimer){
+                enemy.GetComponent<PlayerMovement>().TakeDamage(damage);
+                Debug.Log("hit " + enemy.name);
+                nextAttackTimer = Time.time + cooldownTimer;
+            }
         }
     }
 }
